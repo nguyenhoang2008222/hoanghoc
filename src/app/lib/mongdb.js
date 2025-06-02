2b237e8857a20e7dd21514ffe5690635834c9d84
nguyenhoang2008222 <nguyenroai34@gmail.com>
2025-06-03 02:01:33 +0700

    Add files via upload

--- src/app/lib/mongdb.js
+++ src/app/lib/mongdb.js
@@ -1,16 +1,22 @@
-import mongoose from 'mongoose';
+import { MongoClient } from 'mongodb';
 
-const connectToMongoDB = async () => {
-    try{
-        const uri = process.env.MONGODB_URI;
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-     }
-    };
 
-    export default connectToMongoDB;
+const uri = process.env.MONGODB_URI; // your MongoDB connection string
+const options = {
+};
+
+
+
+
+let client;
+let clientPromise;
+
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable');
+}
+
+
+client = new MongoClient(uri, options);
+clientPromise = client.connect();
+export default clientPromise;
